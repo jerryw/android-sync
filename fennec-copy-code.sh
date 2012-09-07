4d5ba4533787bca0425f1c10c1031595ef7d56bf
Richard Newman <rnewman@twinql.com>
2012-09-07 15:32:57 -0700

    Bug 793053 - Part 4: copy-script changes.

--- fennec-copy-code.sh
+++ fennec-copy-code.sh
@@ -14,13 +14,16 @@ echo "Copying manifests..."
 rsync -a manifests $SYNC/
 
 echo "Copying sources. All use of R must be compiled with Fennec."
-SOURCEDIR="src/main/java/org/mozilla/gecko/sync"
-SOURCEFILES=$(find "$SOURCEDIR" -name '*.java' -not -name 'GlobalConstants.java' -and -not -name 'BrowserContract.java' | sed "s,$SOURCEDIR/,sync/,")
-rsync -C --exclude 'GlobalConstants.java' --exclude 'BrowserContract.java' --exclude '*.in' -a $SOURCEDIR $ANDROID/base/
+SOURCEROOT="src/main/java/org/mozilla/gecko"
+SYNCSOURCEDIR="$SOURCEROOT/sync"
+BACKGROUNDSOURCEDIR="$SOURCEROOT/background"
+SOURCEFILES=$(find "$BACKGROUNDSOURCEDIR" "$SYNCSOURCEDIR" -name '*.java' -not -name 'GlobalConstants.java' -and -not -name 'BrowserContract.java' | sed "s,$SOURCEROOT/,,")
+rsync -C --exclude 'GlobalConstants.java' --exclude 'BrowserContract.java' --exclude '*.in' -a $SYNCSOURCEDIR $ANDROID/base/
+rsync -C --exclude '*.in' -a $BACKGROUNDSOURCEDIR $ANDROID/base/
 
 echo "Copying preprocessed GlobalConstants file."
 PREPROCESS_FILES="sync/GlobalConstants.java"
-cp $SOURCEDIR/GlobalConstants.java.in $ANDROID/base/sync/
+cp $SYNCSOURCEDIR/GlobalConstants.java.in $ANDROID/base/sync/
 
 echo "Copying preprocessed sync_authenticator.xml."
 cp sync_authenticator.xml.template $ANDROID/base/resources/xml/sync_authenticator.xml.in
